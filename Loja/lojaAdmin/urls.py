7ee42c03c0df77265999b3d3a5d482229d4412c1
José Luan <luan.ribeiro@escolar.ifrn.edu.br>
2026-04-13 10:19:33 -0300

    Instalando o pillow

--- Loja/lojaAdmin/urls.py
+++ Loja/lojaAdmin/urls.py
@@ -1,3 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+path('admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 """
 URL configuration for lojaAdmin project.
 
@@ -14,8 +23,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
